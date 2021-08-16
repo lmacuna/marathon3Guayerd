c717b19b0628115cf2c70439bdddc9ad3ee4c616
lucas <lucasmacuna@gmail.com>
2021-08-16 17:45:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,219 @@
+let seguir=true;
+let total = 500;
+let fulll = 200;
+let pokerr = 300;
+let esca = 500;
+let gene = 700;
+let na = 100;
+console.log("Inicial: " + total);
+document.querySelector("#inicio").innerHTML='<p style="font-family:cursive;font-weight:bold;font-size:25px;margin-top:0px;color:whitesmoke">'+`Inicial | ${total}`+'</p>'
+
+const jugar=()=>{
+  if(seguir===true&&total!==0){
+  let n = Math.floor(Math.random() * 6 + 1);
+
+  let n2 =  Math.floor(Math.random() * 6 + 1);
+
+  let n3 =  Math.floor(Math.random() * 6 + 1);
+  let n4 = Math.floor(Math.random() * 6 + 1);
+  let n5 = Math.floor(Math.random() * 6 + 1);
+  
+  //5 switch con img  
+  switch(n){
+          case 1:
+            document.querySelector("#jugada").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado1Blanco.png">'
+            break;
+          case 2:
+            document.querySelector("#jugada").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado2Blanco.png">'
+            break;
+          case 3: 
+            document.querySelector("#jugada").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado3Blanco.png">'
+            break;
+          case 4:
+            document.querySelector("#jugada").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado4Blanco.png">'
+            break;
+          case 5:
+            document.querySelector("#jugada").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado5Blanco.png">'
+            break;
+          case 6:
+            document.querySelector("#jugada").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado6Blanco.png">'
+            break;  
+
+  }
+ 
+switch(n2){
+  case 1:
+    document.querySelector("#jugada2").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado1Blanco.png">'
+    break;
+  case 2:
+    document.querySelector("#jugada2").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado2Blanco.png">'
+    break;
+  case 3: 
+    document.querySelector("#jugada2").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado3Blanco.png">'
+    break;
+  case 4:
+    document.querySelector("#jugada2").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado4Blanco.png">'
+    break;
+  case 5:
+    document.querySelector("#jugada2").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado5Blanco.png">'
+    break;
+  case 6:
+    document.querySelector("#jugada2").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado6Blanco.png">'
+    break;  
+
+}
+switch(n3){
+  case 1:
+    document.querySelector("#jugada3").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado1Blanco.png">'
+    break;
+  case 2:
+    document.querySelector("#jugada3").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado2Blanco.png">'
+    break;
+  case 3: 
+    document.querySelector("#jugada3").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado3Blanco.png">'
+    break;
+  case 4:
+    document.querySelector("#jugada3").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado4Blanco.png">'
+    break;
+  case 5:
+    document.querySelector("#jugada3").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado5Blanco.png">'
+    break;
+  case 6:
+    document.querySelector("#jugada3").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado6Blanco.png">'
+    break;  
+
+}
+switch(n4){
+  case 1:
+    document.querySelector("#jugada4").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado1Blanco.png">'
+    break;
+  case 2:
+    document.querySelector("#jugada4").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado2Blanco.png">'
+    break;
+  case 3: 
+    document.querySelector("#jugada4").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado3Blanco.png">'
+    break;
+  case 4:
+    document.querySelector("#jugada4").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado4Blanco.png">'
+    break;
+  case 5:
+    document.querySelector("#jugada4").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado5Blanco.png">'
+    break;
+  case 6:
+    document.querySelector("#jugada4").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado6Blanco.png">'
+    break;  
+
+}
+switch(n5){
+  case 1:
+    document.querySelector("#jugada5").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado1Blanco.png">'
+    break;
+  case 2:
+    document.querySelector("#jugada5").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado2Blanco.png">'
+    break;
+  case 3: 
+    document.querySelector("#jugada5").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado3Blanco.png">'
+    break;
+  case 4:
+    document.querySelector("#jugada5").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado4Blanco.png">'
+    break;
+  case 5:
+    document.querySelector("#jugada5").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado5Blanco.png">'
+    break;
+  case 6:
+    document.querySelector("#jugada5").innerHTML= '<img style="width:80px;height:80px" src="IMG/dado6Blanco.png">'
+    break;  
+
+}
+  
+/*   alert(
+      "[" + n + "] " + " [" + n2 + "] " + " [" + n3 + "] " + " [" + n4 + "] " + " [" + n5
+          + "] "); */
+
+  var full =
+      (n == n2 && n == n5)
+          || (n == n2 && n == n3 && n != n4 && n != n5 && n4 != n3)
+          || (n2 == n3 && n3 == n4 && n2 != n && n2 != n5)
+          || (n3 == n4 && n4 == n5 && n3 != n && n3 != n2)
+          || (n == n4 && n == n5 && n4 != n3 && n5 != n2)
+          || (n2 == n4 && n2 == n5 && n2 != n && n2 != n3)
+          || (n == n3 && n == n5)
+          || (n == n2 && n == n4)
+          || (n == n2 && n == n4 && n != n3 && n != n5)
+          || (n == n3 && n == n4)
+          || (n2 == n3 && n3 == n5 && n2 != n && n2 != n);
+  var poker =
+      (n == n2 && n2 == n3 && n3 == n4 && n4 != n5)
+          || (n2 == n3 && n3 == n4 && n4 == n5 && n5 != n)
+          || (n3 == n4 && n4 == n5 && n5 == n && n5 != n2)
+          || (n4 == n5 && n5 == n && n == n2 && n2 != n3)
+          || (n5 == n && n == n2 && n2 == n3 && n3 != n4);
+  var generala = (n == n2 && n2 == n3 && n3 == n4 && n4 == n5);
+  //var jugada = ((poker) || (full) || (generala));
+  var escalera =
+      ((poker == false) && (full == false) && (generala == false))
+          && (n == 3 || n == 4 || n == 5 || n2 == 3 || n2 == 4 || n2 == 5 || n3 == 3 || n3 == 4
+              || n3 == 5 || n4 == 3 || n4 == 4 || n4 == 5 || n5 == 3 || n5 == 4 || n5 == 5)
+          && (n != n2 && n != n3 && n != n4 && n != n5)
+          && (n2 != n && n2 != n3 && n2 != n4 && n2 != n5)
+          && (n3 != n && n3 != n2 && n3 != n4 && n3 != n5)
+          && (n4 != n && n4 != n2 && n4 != n3 && n4 != n5)
+          && (n5 != n && n5 != n2 && n5 != n3 && n5 != n4)
+          && (n == 5 || n2 == 5 || n3 == 5 || n4 == 5 || n5 == 5)
+          && (n == 3 || n2 == 3 || n3 == 3 || n4 == 3 || n5 == 3)
+          && (n == 4 || n2 == 4 || n3 == 4 || n4 == 4 || n5 == 4);
+  // full 3 iguales
+  // poker 4 iguales
+  // generala 5 iguales
+  // escaleras
+  /* {1,2,3,4,5}; {2,3,4,5,6}; {3,4,5,6,1}*/
+
+  if (full && poker == false && generala == false) {
+    total = total + fulll;
+    document.querySelector("#tipoJugada").innerHTML='<p>'+`FULL | ${fulll}`+'</p>'
+    //alert("\n full " + fulll);
+  }
+  if (poker && generala == false) {
+    total = total + pokerr;
+    document.querySelector("#tipoJugada").innerHTML='<p>'+`POKER | ${pokerr}`+'</p>'
+   // alert("\n poker " + pokerr);
+  }
+  if (generala) {
+    total = total + gene;
+    document.querySelector("#tipoJugada").innerHTML='<p>'+`GENERALA | ${gene}`+'</p>'
+   // alert("\n generala " + gene);
+  }
+  if (escalera && full == false && poker == false && generala == false) {
+    total = total + esca;
+    document.querySelector("#tipoJugada").innerHTML='<p>'+`ESCALERA | ${esca}`+'</p>'
+   // alert("\n escalera " + esca);
+  }
+  if (full == false && poker == false && generala == false && escalera == false) {
+    total = total - na;
+    document.querySelector("#tipoJugada").innerHTML='<p>'+`NADA | -${na}`+'</p>'
+    //alert("\nNada -" + na + "\n");
+    if(total===0){
+      seguir=false;
+      document.querySelector("#mensaje").innerHTML='<p>'+`FIN DEL JUEGO `+'</p>'
+      document.querySelector("#denuevo").innerHTML='<button style="width:110px !important;font-size:18px !important" onclick="otravez()">'+"Jugar de nuevo"+'</button>'
+      
+    }
+  }
+  
+  //seguir=parseInt(prompt("seguir jugando?\n1-si\n2-salir\n"));
+
+document.querySelector("#subtotal").innerHTML='<p>'+`PUNTOS ACUMULADOS ${total}`+'</P>'
+} 
+}
+
+
+const otravez=()=>{
+  document.querySelector("#mensaje").innerText=""
+  document.querySelector("#denuevo").innerHTML=""
+  document.querySelector("#tipoJugada").innerHTML=""
+  document.querySelector("#subtotal").innerHTML=""
+  seguir=true;
+  total=500;
+ 
+ 
+}
