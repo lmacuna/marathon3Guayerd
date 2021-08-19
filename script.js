3337e7320fb0db72de4cd0e3696e9ec0db7fc4de
lucas <lucasmacuna@gmail.com>
2021-08-19 19:14:08 -0300

    Configuration 7

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ let esca = 500;
 let gene = 1000;
 let na = 100;
 console.log("Inicial: " + total);
+
 document.querySelector("#inicio").innerHTML='<p style="font-family:cursive;font-weight:bold;font-size:25px;margin-top:0px;color:whitesmoke">'+`Inicial | ${total}`+'</p>'
 document.querySelector("#tipojugada1").innerHTML='<p>'+"A JUGAR"+'</p>'
+const sonido=()=>{
+  if(total!==0&&seguir===true){
+    document.querySelector("#cortina").innerHTML='<audio autoplay="autoplay" loop="loop">'+'<source  src="cortinamusical.mp3"type="audio/mp3"/>'+'</audio>'
+  }
 
+}
 const jugar=()=>{
   if(seguir===true&&total!==0){
     document.querySelector("#sonido").innerHTML='<audio autoplay>'+'<source src="sonidoDados2.mp3" type="audio/mpeg">'+'</audio>'
@@ -184,7 +190,7 @@ switch(n5){
   }
   if (poker && generala == false) {
     total = total + pokerr;
-    document.querySelector("#sonido").innerHTML='<audio autoplay>'+'<source src="premioMayor.mp3" type="audio/mpeg">'+'</audio>'
+    document.querySelector("#sonido").innerHTML='<audio autoplay>'+'<source src="felicidades.mp3" type="audio/mpeg">'+'</audio>'
     document.querySelector("#tipoJugada").innerHTML='<p>'+`POKER | ${pokerr}`+'</p>'
     document.querySelector("#tablero").classList.replace("dados","poker")
     document.querySelector("#tablero").classList.replace("full","poker")
@@ -231,6 +237,8 @@ switch(n5){
       document.querySelector("#mensaje").innerHTML='<p>'+`FIN DEL JUEGO `+'</p>'
       document.querySelector("#denuevo").innerHTML='<button style="width:110px !important;font-size:18px !important" onclick="otravez()">'+"Jugar de nuevo"+'</button>'
       document.querySelector("body").classList.add("perder")
+      document.querySelector("#cortina").innerHTML='<audio muted="muted">'+'<source src="" type="audio/mpeg">'+'</audio>'
+      
     }
   }
   
